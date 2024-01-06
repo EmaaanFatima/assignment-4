1261414011c9cfd6cf00768c4265ffbb1dfe325b
Zohaib11117 <105278831+Zohaib11117@users.noreply.github.com>
2024-01-07 01:00:25 +0500

    first commit

---
+++ dart/main.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'dart:mirrors';
+
+void main() {
+// Q1.Write a program that takes a list
+// of numbers as input and prints the even numbers in the list using a for loop.
+
+// Example:
+
+// Input:
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Output:
+// 2 4 6 8 10
+  print("Question # 1");
+  List mytable = ["1,2,3,4,5,6,7,8,9,10"];
+  for (var i = 2; i <= 10; i++ + ++i) {
+    print(i);
+  }
+//   Q2.Write a program that prints the
+// Fibonacci sequence up to a given number using a for loop.
+
+// Example:
+
+// Input:
+// 10
+
+// Output:
+// 0 1 1 2 3 5 8
+  print("Question#2");
+  List num = ["10"];
+  for (var i = 0; i <= 1; ++i + i++ ; i = 1; ) {
+    print(i);
+  }
+}
